996f9e8a10865eada297b868b27b30709ff74627
Jeff Gradner <gradnerj@gmail.com>
2020-11-25 17:34:58 -0700

    Updated to support TF model

--- metrics.py
+++ metrics.py
@@ -1,10 +1,17 @@
 import pandas as pd
 import numpy as np
+import tensorflow as tf
 
 def get_metrics(model, X, y, name):
     metrics = {}
     metrics['Name'] = name
-    cm = pd.crosstab(model.predict(X).ravel(), y.values.ravel(),rownames=['pred'], colnames=['actual'])
+    if type(model) == tf.python.keras.engine.training.Model:
+        y_pred = model.predict(X)
+        y_pred = np.where(y_pred>=0.7, 1, y_pred)
+        y_pred = np.where(y_pred<0.7, 0, y_pred)
+        cm = pd.crosstab(y_pred.ravel(), y.values.ravel(), rownames=['pred'], colnames=['actual'])
+    else:    
+        cm = pd.crosstab(model.predict(X).ravel(), y.values.ravel(),rownames=['pred'], colnames=['actual'])
     metrics['CM'] = cm
     metrics['Specificity'] = cm[0][0] / (cm[0][0]+cm[0][1])
     metrics['Sensitivity'] = cm[1][1] / (cm[1][1] + cm[1][0])
